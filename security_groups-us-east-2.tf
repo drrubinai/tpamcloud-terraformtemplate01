38f88abe511e3b5b705a4f80b24b5b4ccf604362
Irfan Ilyas <irfan_ilyas@epam.com>
2020-10-01 10:19:08 -0400

    add a new sg sg-33333b

--- security_groups-us-east-2.tf
+++ security_groups-us-east-2.tf
@@ -1,4 +1,46 @@
 
+resource "aws_security_group" "sg-333333b" {
+     description  = "default VPC security group"
+     egress       = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+
+     ingress      = [
+        {
+            cidr_blocks      = [
+                "95.26.9.211/32",
+	            ]
+            description      = "For SQL"
+            from_port        = 1433
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 1433
+        },
+   ]
+     name         = "default"
+     tags         = {}
+     vpc_id       = "vpc-8fc83ee6"
+
+    timeouts {}
+}
+
+
+
 resource "aws_security_group" "sg-023ac37b" {
      description  = "default VPC security group"
      egress       = [
