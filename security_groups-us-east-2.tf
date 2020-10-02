ecf0d49fb36564e52d76e2c70c67185f06ce32f5
Irfan Ilyas <irfan_ilyas@epam.com>
2020-10-01 23:32:09 -0400

    testing with pass fail SGs 2

--- security_groups-us-east-2.tf
+++ security_groups-us-east-2.tf
@@ -1,4 +1,48 @@
+#failing - having  "93.23.3.213/32", (invalid cidr) in ingress
+resource "aws_security_group" "sg-222222b" {
+     description  = "default VPC security group"
+     egress       = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
 
+     ingress      = [
+        {
+            cidr_blocks      = [
+                "95.26.9.211/32",
+				"93.23.3.213/32",
+			    "89.223.214.96/28", 
+			    "79.134.76.56/30",				
+	            ]
+            description      = "For SQL"
+            from_port        = 1433
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 1433
+        },
+   ]
+     name         = "default"
+     tags         = {}
+     vpc_id       = "vpc-8fc83ee6"
+
+    timeouts {}
+}
+
+#passing - having  all cidrs exist in valid_cidr list in ingress
 resource "aws_security_group" "sg-333333b" {
      description  = "default VPC security group"
      egress       = [
@@ -42,6 +86,7 @@ resource "aws_security_group" "sg-333333b" {
     timeouts {}
 }
 
+#failing - having  an unknown cidr not found in valid-cidr-list in ingress
 resource "aws_security_group" "sg-44444b" {
      description  = "default VPC security group"
      egress       = [
