91c25d9ba60da6128e0f35e4b12406d23fea79f6
Irfan Ilyas <irfan_ilyas@epam.com>
2020-10-01 23:09:41 -0400

    testing pass fail SGs

--- security_groups-us-east-2.tf
+++ security_groups-us-east-2.tf
@@ -21,10 +21,9 @@ resource "aws_security_group" "sg-333333b" {
         {
             cidr_blocks      = [
                 "95.26.9.211/32",
-			"95.26.70.0/24", 
-			"89.223.214.64/27", 
-			"89.223.214.96/28", 
-			"79.134.76.56/30",				
+			    "95.26.70.0/24", 
+			    "89.223.214.96/28", 
+			    "79.134.76.56/30",				
 	            ]
             description      = "For SQL"
             from_port        = 1433
@@ -43,3 +42,46 @@ resource "aws_security_group" "sg-333333b" {
     timeouts {}
 }
 
+resource "aws_security_group" "sg-44444b" {
+     description  = "default VPC security group"
+     egress       = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+
+     ingress      = [
+        {
+            cidr_blocks      = [
+                "95.26.9.211/32",
+			    "95.26.70.0/24", 
+			    "96.66.70.0/24", 				
+			    "89.223.214.96/28", 
+			    "79.134.76.56/30",				
+	            ]
+            description      = "For SQL"
+            from_port        = 1433
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 1433
+        },
+   ]
+     name         = "default"
+     tags         = {}
+     vpc_id       = "vpc-8fc83ee6"
+
+    timeouts {}
+}
